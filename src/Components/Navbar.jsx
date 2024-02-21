0c01753b74d0610fc0ecfb1b76935f14651683e1
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-02-21 23:10:37 +0530

    organize and export components in main entry file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -74,3 +74,5 @@ const Navbar = () => {
         </>
     );
 };
+
+export default Navbar;
---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import FeaturesCard from "./FeaturesCard";
+import ProductCard from "./ProductCard";
+
+export { Navbar, Footer, FeaturesCard, ProductCard}
