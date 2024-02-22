d4ac6e89845943e640ed760294fc140b7f3a7522
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-02-22 22:08:03 +0530

    refaractor component exports in index.js for enhanced organization and clarity.

---
+++ src/Container/index.js
@@ -0,0 +1,8 @@
+import Hero from "./Hero";
+import Features from "./Features";
+import Lamps from "./Lamps";
+import Trending from "./Trending";
+import Signin from "./Signin";
+import About from "./About";
+
+export { Hero, Features, Lamps, Trending, Signin, About };
